88d5427eeded89e8a7584c88e94ed7f172de1fe6
JYOTIRANJAN MUDULI <92794275+jyotimuduli3@users.noreply.github.com>
2023-04-07 02:34:38 +0530

    Initial commit

---
+++ src/components/Card.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+function Card(props) {
+  return (
+    <div>
+      <h1>My Contacts</h1>
+
+      <h2>{props.name}</h2>
+      <img src={props.img} alt="avatar_img" />
+      <p>{props.tel}</p>
+      <p>{props.email}</p>
+    </div>
+  );
+}
+export default Card;
